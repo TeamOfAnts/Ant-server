d1839452bd47e14cbba9d0f8294eab1101c3620c
안유진 <yoojin.an0@gmail.com>
2024-12-18 11:51:26 +0900

    Fix: api 스팩 변경 및 테스트코드 추가

---
+++ src/test/kotlin/com/example/antserver/application/SchedulerTest.kt
@@ -0,0 +1,29 @@
+//package com.example.antserver.application
+//
+//class SchedulerTest {
+//    @Test
+//    @DisplayName("일요일 6시에 모각코 투표가 생성된다")
+//    fun () {
+//        // given
+//
+//
+//        // when
+//
+//
+//        // then
+//
+//    }
+//
+//    @Test
+//    @DisplayName("수요일 6시에 모각코 스케쥴이 확정된다")
+//    fun () {
+//        // given
+//
+//
+//        // when
+//
+//
+//        // then
+//
+//    }
+//}
