65e30b692e186274f0206c4ea01705804f0d90de
안유진 <yoojin.an0@gmail.com>
2024-12-22 14:39:34 +0900

    Fix: cors 비활성화

--- src/main/kotlin/com/example/antserver/util/config/SecurityConfig.kt
+++ src/main/kotlin/com/example/antserver/util/config/SecurityConfig.kt
@@ -1,8 +1,8 @@
 package com.example.antserver.util.config
 
-import com.example.antserver.util.exception.GlobalExceptionHandler
 import com.example.antserver.util.exception.CustomAccessDeniedHandler
 import com.example.antserver.util.exception.CustomAuthenticationEntryPoint
+import com.example.antserver.util.exception.GlobalExceptionHandler
 import com.example.antserver.util.jwt.JwtAuthenticationFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -12,6 +12,10 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.web.SecurityFilterChain
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
+import org.springframework.web.cors.CorsConfiguration
+import org.springframework.web.cors.CorsConfigurationSource
+import java.util.*
+
 
 @Configuration
 @EnableWebSecurity
@@ -20,6 +24,18 @@ class SecurityConfig(
     private val customAccessDeniedHandler: CustomAccessDeniedHandler,
     private val jwtAuthenticationFilter: JwtAuthenticationFilter,
 ) {
+
+    fun corsConfigurationSource(): CorsConfigurationSource {
+        return CorsConfigurationSource { request ->
+            val config = CorsConfiguration()
+            config.allowedHeaders = Collections.singletonList("*")
+            config.allowedMethods = Collections.singletonList("*")
+            config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:8888"))
+            config.allowCredentials = true
+            config
+        }
+    }
+
     @Bean
     @Throws(Exception::class)
     fun filterChain(http: HttpSecurity, globalExceptionHandler: GlobalExceptionHandler): SecurityFilterChain {
@@ -27,7 +43,7 @@ class SecurityConfig(
             .httpBasic { it.disable() }
             .formLogin { it.disable() }
             .headers { it.frameOptions { frameOptions -> frameOptions.sameOrigin() } }
-            .cors { it.disable() } // CORS 에러 방지용 TODO. 도메인 추가
+            .cors { corsConfigurer -> corsConfigurer.configurationSource(corsConfigurationSource()) } // CORS 에러 방지용 TODO. 도메인 추가
             .sessionManagement { it.sessionCreationPolicy(SessionCreationPolicy.STATELESS) } // Stateless 세션 설정
             .authorizeHttpRequests { auth ->
                 auth.requestMatchers(HttpMethod.OPTIONS).permitAll() // CORS Preflight 방지
