76216111e9b10daa19ac8049d088623da25cf1fc
안유진 <yoojin.an0@gmail.com>
2024-12-23 23:22:37 +0900

    Refactor: 모든 ApplicationException은 GlobalExceptionHandler가 처리하도록 수정

--- src/main/kotlin/com/example/antserver/util/exception/AuthenticationException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.antserver.util.exception
-
-class AuthenticationException(message: String?): BaseException(message)
--- src/main/kotlin/com/example/antserver/util/exception/BaseException.kt
+++
@@ -1,3 +0,0 @@
-package com.example.antserver.util.exception
-
-open class BaseException(override val message: String?): RuntimeException(message)
