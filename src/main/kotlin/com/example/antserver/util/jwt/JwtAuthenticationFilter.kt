bb655effd869d861e48c793b927706b5b309c04e
안유진 <yoojin.an0@gmail.com>
2024-12-31 16:29:14 +0900

    Refactor: filter chain 내부에서 error 발생 시 응답 생성 함수 공용화

--- src/main/kotlin/com/example/antserver/util/jwt/JwtAuthenticationFilter.kt
+++ src/main/kotlin/com/example/antserver/util/jwt/JwtAuthenticationFilter.kt
@@ -12,6 +12,7 @@ import com.example.antserver.domain.user.UserRepository
 import com.example.antserver.util.exception.ApplicationException
 import com.example.antserver.util.exception.AuthenticationException
 import com.example.antserver.util.response.Status
+import com.example.antserver.util.response.writeErrorResponse
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.core.context.SecurityContextHolder
 
@@ -32,8 +33,8 @@ class JwtAuthenticationFilter(
             tokenService.isTokenValid(accessToken)
             authenticateUser(accessToken)
         } catch (exception: AuthenticationException) {
-            // NOTE: access token 만료 에러의 경우 refresh 해야하기 때문에 errorResponse를 return한다.
-            writeJsonErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, exception.message)
+            // NOTE: access token 만료 에러의 경우 refresh 해야하기 때문에 에러 응답을 생성한다.
+            writeErrorResponse(response, HttpServletResponse.SC_UNAUTHORIZED, exception.message)
             return
         } catch (exception: NullPointerException) {
             // NOTE: access token 없이 허용된 요청도 filter chain 밖으로 에러가 전파되기 때문에 catch해서 doFilter한다.
@@ -59,24 +60,5 @@ class JwtAuthenticationFilter(
             userDetails.authorities
         )
         SecurityContextHolder.getContext().authentication = authentication
-    }
-
-    fun writeJsonErrorResponse(
-        response: HttpServletResponse,
-        status: Int,
-        errorMessage: String
-    ) {
-        response.contentType = "application/json"
-        response.characterEncoding = "UTF-8"
-        response.status = status
-        response.writer.write("""
-        {
-            "data": {
-                "errorMessage": "$errorMessage"
-            }
-        }
-        """.trimIndent()
-        )
-        response.writer.flush()
     }
 }
--- src/main/kotlin/com/example/antserver/util/response/ExceptionResponse.kt
+++ src/main/kotlin/com/example/antserver/util/response/ExceptionResponse.kt
@@ -1,5 +1,21 @@
 package com.example.antserver.util.response
 
+import jakarta.servlet.http.HttpServletResponse
+
 data class ExceptionResponse(
     val errorMessage: String
 )
+
+fun writeErrorResponse(response: HttpServletResponse, status: Int, errorMessage: String) {
+    response.contentType = "application/json"
+    response.characterEncoding = "UTF-8"
+    response.status = status
+    response.writer.write("""
+        {
+            "data": {
+                "errorMessage": "$errorMessage"
+            }
+        }
+    """.trimIndent())
+    response.writer.flush()
+}
