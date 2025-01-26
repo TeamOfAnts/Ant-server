0b217a8bd3dc75b4f207fbb2f374d9df0870ac84
안유진 <yoojin.an0@gmail.com>
2025-01-26 23:56:38 +0900

    Test: FakeRepository 추가
    - Bean 등록

---
+++ src/test/kotlin/com/example/antserver/fake/FakeUserRepository.kt
@@ -0,0 +1,40 @@
+package com.example.antserver.fake
+
+import com.example.antserver.domain.user.ProviderType
+import com.example.antserver.domain.user.User
+import com.example.antserver.domain.user.UserRepository
+import com.fasterxml.uuid.Generators
+import java.util.*
+
+class FakeUserRepository: UserRepository {
+     val data: MutableList<User> = mutableListOf()
+
+    override fun save(user: User): User {
+        val existingIndex = data.indexOfFirst { it.id == user.id }
+
+        return if (existingIndex != -1) {
+            data[existingIndex] = user
+            user
+        } else {
+            val newUser = user.copy(id = Generators.timeBasedEpochGenerator().generate())
+            data.add(newUser)
+            newUser
+        }
+    }
+
+    fun saveAll(users: List<User>) {
+        data.addAll(users)
+    }
+
+    override fun findById(userId: UUID): User? {
+        return data.find { it.id == userId }
+    }
+
+    override fun findByEmailAndProvider(email: String, provider: ProviderType): User? {
+        return data.find { it.email == email && it.provider == provider }
+    }
+
+    fun clear() {
+        data.clear()
+    }
+}
