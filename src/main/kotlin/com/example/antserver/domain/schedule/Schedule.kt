053941b9afe45bf67f4d8bfa5b5215f4dcd0525a
안유진 <yoojin.an0@gmail.com>
2025-01-28 01:26:01 +0900

    Feat: 주말 Schedule은 낮/밤 옵션 추가
    - ScheduleOn.Scheduled 타입을 Instant?에서 String?으로 변경
    - ScheduleOn.Scheduled의 필드를 schedule(Instant)에서 date(LocalDate), dayOfWeek(DayOfWeek), timeOfDay(String?)으로 변경
    - Schedule.scheduleOn은 ScheduleOn.Scheduled의 메소드 toString을 통해 '0000-00-00 (요일) 낮'형태로 저장
    - dayOfWeek을 한국 요일로 매핑하는 함수 추가

--- src/main/kotlin/com/example/antserver/domain/schedule/Schedule.kt
+++ src/main/kotlin/com/example/antserver/domain/schedule/Schedule.kt
@@ -4,7 +4,10 @@ import com.example.antserver.domain.AggregateRoot
 import com.example.antserver.domain.user.User
 import com.example.antserver.infrastructure.schedule.ScheduleOnConverter
 import jakarta.persistence.*
+import java.time.DayOfWeek
 import java.time.Instant
+import java.time.LocalDate
+import java.time.ZoneId
 import java.time.temporal.ChronoUnit
 import java.util.*
 
@@ -35,19 +38,25 @@ data class Schedule(
     companion object {
         fun ofSchedules(pollId: Long, startDate: Instant, daysWithUnscheduled: Long): List<Schedule> {
             val schedules = mutableListOf<Schedule>()
-            (0 until daysWithUnscheduled + 1).map { days ->
-                val scheduleOn = if (days < daysWithUnscheduled) {
-                    ScheduleOn.Scheduled(
-                        startDate.plus(1, ChronoUnit.DAYS)
-                    )
-                } else {
-                    ScheduleOn.Unscheduled
-                }
-                schedules.add(
-                    Schedule(
-                        pollId = pollId,
-                        scheduleOn = scheduleOn
-                    )
+            val seoulZoneId = ZoneId.of("Asia/Seoul")
+
+            (0 until daysWithUnscheduled).forEach { day ->
+                val currentDate = startDate.plus(day, ChronoUnit.DAYS).atZone(seoulZoneId).toLocalDate()
+                val dayOfWeek = currentDate.dayOfWeek
+
+                schedules.addAll(
+                    when {
+                        day < daysWithUnscheduled - 1 && (dayOfWeek == DayOfWeek.SATURDAY || dayOfWeek == DayOfWeek.SUNDAY) -> listOf(
+                            Schedule(pollId = pollId, scheduleOn = ScheduleOn.Scheduled(currentDate, dayOfWeek, "낮")),
+                            Schedule(pollId = pollId, scheduleOn = ScheduleOn.Scheduled(currentDate, dayOfWeek, "저녁"))
+                        )
+                        day < daysWithUnscheduled - 1 -> listOf(
+                            Schedule(pollId = pollId, scheduleOn = ScheduleOn.Scheduled(currentDate, dayOfWeek))
+                        )
+                        else -> listOf(
+                            Schedule(pollId = pollId, scheduleOn = ScheduleOn.Unscheduled)
+                        )
+                    }
                 )
             }
             return schedules
@@ -75,6 +84,35 @@ data class Schedule(
 
 @Embeddable
 sealed class ScheduleOn {
-    data class Scheduled(val schedule: Instant) : ScheduleOn()
+    abstract override fun toString(): String
+
+    companion object {
+        private val koreanDays = mapOf(
+            DayOfWeek.MONDAY to "월",
+            DayOfWeek.TUESDAY to "화",
+            DayOfWeek.WEDNESDAY to "수",
+            DayOfWeek.THURSDAY to "목",
+            DayOfWeek.FRIDAY to "금",
+            DayOfWeek.SATURDAY to "토",
+            DayOfWeek.SUNDAY to "일"
+        )
+        fun getKoreanDay(dayOfWeek: DayOfWeek): String {
+            return koreanDays[dayOfWeek]!!
+        }
+    }
+
+    data class Scheduled(
+        val date: LocalDate,
+        val dayOfWeek: DayOfWeek,
+        val timeOfDay: String? = null) : ScheduleOn() {
+            override fun toString(): String {
+                return listOfNotNull(
+                    date.toString(),
+                    "(${getKoreanDay(dayOfWeek)})",
+                    timeOfDay
+                ).joinToString(" ")
+            }
+        }
+
     data object Unscheduled : ScheduleOn()
 }
--- src/main/kotlin/com/example/antserver/infrastructure/schedule/ScheduleOnConverter.kt
+++ src/main/kotlin/com/example/antserver/infrastructure/schedule/ScheduleOnConverter.kt
@@ -3,22 +3,27 @@ package com.example.antserver.infrastructure.schedule
 import com.example.antserver.domain.schedule.ScheduleOn
 import jakarta.persistence.AttributeConverter
 import jakarta.persistence.Converter
-import java.time.Instant
+import java.time.DayOfWeek
+import java.time.LocalDate
 
 @Converter(autoApply = true)
-class ScheduleOnConverter : AttributeConverter<ScheduleOn, Instant> {
-    override fun convertToDatabaseColumn(attribute: ScheduleOn): Instant? {
+class ScheduleOnConverter : AttributeConverter<ScheduleOn, String> {
+    override fun convertToDatabaseColumn(attribute: ScheduleOn): String? {
         return when (attribute) {
-            is ScheduleOn.Scheduled -> attribute.schedule
+            is ScheduleOn.Scheduled -> attribute.toString()
             ScheduleOn.Unscheduled -> null
         }
     }
 
-    override fun convertToEntityAttribute(dbData: Instant?): ScheduleOn {
+    override fun convertToEntityAttribute(dbData: String?): ScheduleOn {
         return if (dbData == null) {
             ScheduleOn.Unscheduled
         } else {
-            ScheduleOn.Scheduled(dbData)
+            val parts = dbData.split(" ")
+            val date = LocalDate.parse(parts[0])
+            val dayOfWeek = DayOfWeek.valueOf(parts[1])
+            val timeOfDay = parts.getOrNull(2)
+            ScheduleOn.Scheduled(date, dayOfWeek, timeOfDay)
         }
     }
 }
